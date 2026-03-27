c86005a40be49381de1d90cc49376e5ac1b11e72
kal-kyokya <kalkyokya4@gmail.com>
2026-03-27 13:19:40 +0000

    Customize the programs' admin panel display

--- backend/apps/programs/admin.py
+++ backend/apps/programs/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Program
 
-# Register your models here.
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'level', 'delivery_format', 'is_published', 'is_featured')
+    list_filter = ('is_published', 'is_featured', 'level', 'delivery_format')
+    search_fields = ('title', 'short_description', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+    actions = ['published_selected', 'unpublished_selected']
+
+    @admin.action(description='Publish selected programs')
+    def publish_selected(self, request, queryset):
+        queryset.update(is_published=True)
+
+    @admin.action(description='Unpublish selected programs')
+    def unpublish_selected(self, request, queryset):
+        queryset.update(is_published=False)
