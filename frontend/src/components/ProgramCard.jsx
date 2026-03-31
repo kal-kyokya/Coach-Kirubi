94a63acb59f9af241ea6f190260135f4d8a17546
kal-kyokya <kalkyokya4@gmail.com>
2026-03-31 10:11:16 +0000

    Create the Program Card component

---
+++ frontend/src/components/ProgramCard.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+export default function ProgramCard({ program }) {
+    return (
+	<article className="program-card">
+	    <h3>{program.title}</h3>
+	    <p>{program.short_description}</p>
+	    <div className="meta">
+		<span>{program.duration_weeks} weeks</>
+		<span>{program.level}</span>
+	    </div>
+	    <strong>KES {program.price}</strong>
+	    <Link to={`/programs/{program.slug}` className="cta"}></Link>
+	</article>
+    )
+}
